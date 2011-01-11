3dcebd509f92c61b5067931efddf7278caf0973c
tenman <a.tenman@gmail.com>
2011-01-12 08:42:14 +0900

    modified:   lib/individual-css.php     modified:   style.css

--- lib/individual-css.php
+++ lib/individual-css.php
@@ -8,18 +8,20 @@
         require_once(dirname($config_dir).'/wp-config.php') or die("error");
     }
 
-    $number = $_GET['ver'];
+  /*  $number = $_GET['ver'];
 
     if(!preg_match('|[0-9a-f]{32}-([0-9]{1,})|si',$number,$regs)){
 
         exit;
     }
-    $number = $regs[1];
+    $number = $regs[1];*/
 
-    $table = $table_prefix.$number.'_options';
+
+    $table = $table_prefix.'options';
+
     $link = mysql_connect(DB_HOST,DB_USER,DB_PASSWORD);
     $sdb = mysql_select_db(DB_NAME,$link);
-    $sql = 'SELECT * FROM `wp_4_options` WHERE `option_name` LIKE \'_raindrops_indv_css\'';
+    $sql = 'SELECT * FROM `'.$table.'` WHERE `option_name` LIKE \'_raindrops_indv_css\'';
     $result = mysql_query($sql, $link);
     $rows = mysql_fetch_row($result);
 

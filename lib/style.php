1131e06e9fef6370d55f54d12cdd7f6b1a6117fd
tenman <a.tenman@gmail.com>
2011-01-12 09:40:56 +0900

    The deletion is corrected by mistake

---
+++ lib/style.php
@@ -0,0 +1,47 @@
+<?php
+header("Content-type: text/css");
+$dir = dirname(dirname(dirname(__FILE__)));
+$config_dir = dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+
+    if(file_exists($config_dir.'/wp-config.php')){
+        require_once($config_dir.'/wp-config.php');
+    }else{
+        require_once(dirname($config_dir).'/wp-config.php') or die("error");
+    }
+
+ob_start ("ob_gzhandler");
+echo '@charset "utf-8";';
+echo whitespace_del($dir.'/raindrops/reset-fonts-grids.css');
+echo whitespace_del($dir.'/raindrops/grids.css');
+echo whitespace_del($dir.'/raindrops/fonts.css');
+echo whitespace_del($dir.'/raindrops/style.css');
+
+/*languages*/
+if(file_exists($dir.'/raindrops/languages/css/'.WPLANG.'.css')){
+    echo whitespace_del($dir.'/raindrops/languages/css/'.WPLANG.'.css');
+}
+
+/* child theme style*/
+if(basename(dirname(dirname(__FILE__))) !== "raindrops"){
+    $current_style = preg_replace("|@import[^;]+;|","",whitespace_del('../style.css'));
+
+echo $current_style;
+}
+ob_end_flush();
+
+function whitespace_del($path){
+
+    $handle = fopen($path, "r") or die ("error not open");
+    $buffer = "";
+
+    while (!feof($handle)) {
+        $buffer = fgetss($handle);
+
+        $buffers .= $buffer;
+    }
+    fclose($handle);
+    $buffers = str_replace(array("\r","\n","\t",""),"",$buffers);
+    return $buffers;
+
+}
+?>

d6495e2e4db1955c7b9c1010cdaf590f40794a70
tenman <a.tenman@gmail.com>
2011-02-08 12:35:58 +0900

    add CSS templates

---
+++ lib/templates/minimal.php
@@ -0,0 +1,26 @@
+
+%gradient%
+
+#access ul ul.children a{
+
+background:#fff;
+
+}
+#access .menu  ul li{
+border:1px solid %rgba_border%
+}
+#year_list,
+#year_list td,
+#month_list,
+#month_list td,
+#datelist,
+#date_list td{
+
+border:1px solid %rgba_border%
+
+}
+.footer-widget h2,.rsidebar h2,.lsidebar h2 {
+    %c4%
+    %h2_default_background%;
+    %h_position_rsidebar_h2%
+}

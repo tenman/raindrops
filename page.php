2e7046c27525bec14230f3a928234f8fed61d97d
tenman <a.tenman@gmail.com>
2011-05-27 17:41:09 +0900

    modified:   changelog.txt     modified:   images/headers/wp3-thumbnail.jpg     modified:   images/headers/wp3.jpg     modified:   page.php     modified:   style.css

--- page.php
+++ page.php
@@ -14,18 +14,18 @@
   <div class="yui-b" >
     <div class="<?php echo raindrops_yui_class_modify();?>" id="container">
       <div class="yui-u first" <?php is_2col_raindrops('style="width:99%;"');?>>
-	  
+
 <?php
 /**
  * parent child links
  */
-$raindrops_ancestor 		= array_pop(get_post_ancestors($post->ID)); 
-$raindrops_list_pages 		= wp_list_pages('depth=5&echo=0&child_of=' . $post->ID . '&title_li=<strong>' . __('Pages', 'Raubdrops') . '</strong>' );
-$raindrops_ancestor_list_page = wp_list_pages('depth=5&echo=0&child_of=' . $ancestor . '&title_li=<strong>' . __('Pages', 'Raindrops') . '</strong>' ); 
- 
+$raindrops_ancestor         = array_pop(get_post_ancestors($post->ID));
+$raindrops_list_pages       = wp_list_pages('depth=5&echo=0&child_of=' . $post->ID . '&title_li=<strong>' . __('Pages', 'Raubdrops') . '</strong>' );
+$raindrops_ancestor_list_page = wp_list_pages('depth=5&echo=0&child_of=' . $raindrops_ancestor . '&title_li=<strong>' . __('Pages', 'Raindrops') . '</strong>' );
+
 if(empty($ancestor) and !empty($raindrops_list_pages)) {
 echo '<ul class="rd-page-navigation clearfix root">';
-echo $raindrops_list_pages; 
+echo $raindrops_list_pages;
 echo '</ul>';
 }else {
 echo '<ul class="rd-page-navigation clearfix">';
@@ -34,7 +34,7 @@ echo '</ul>';
 }
 ?>
         <?php if (have_posts()){ ?>
-        <?php 		while (have_posts()){ the_post(); ?>
+        <?php       while (have_posts()){ the_post(); ?>
         <div class="entry page">
           <div id="post-<?php the_ID(); ?>" <?php post_class();?>>
             <h2 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
@@ -56,14 +56,14 @@ echo '</ul>';
             <?php comments_template( '', true ); ?>
           </div>
         </div>
-        <?php 		} //endwhile ?>
-        <?php 		if ( $wp_query->max_num_pages > 1 ){ ?>
+        <?php       } //endwhile ?>
+        <?php       if ( $wp_query->max_num_pages > 1 ){ ?>
         <div id="nav-below" class="clearfix"> <span class="nav-previous">
           <?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'Raindrops' ) ); ?>
           </span> <span class="nav-next">
           <?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'Raindrops' ) ); ?>
           </span> </div>
-        <?php 		} //end max_num_pages > 1 ?>
+        <?php       } //end max_num_pages > 1 ?>
         <?php } //end have post?>
       </div>
       <div class="yui-u"><?php if($rsidebar_show){get_sidebar('extra');} ?></div>

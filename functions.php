0bcf0c12bf21c00e9ffc8659bfe89825f068da26
tenman <a.tenman@gmail.com>
2012-01-06 10:41:39 +0900

    modified:   changelog.txt     modified:   functions.php

--- functions.php
+++ functions.php
@@ -8,6 +8,7 @@
  */
 ?>
 <?php
+/*----- Copy this area and paste Child functions.php when functions customize / Start------*/
     if(!defined('ABSPATH')){
         exit;
     }
@@ -63,201 +64,24 @@
         }elseif(!in_array('alias_functions.php',$included_files)){
             require_once(get_template_directory().'/lib/alias_functions.php');
         }
+
 /**
- *
+ * It has hooks.
  *
  *
  *
  *
  */
-    add_filter( 'use_default_gallery_style', '__return_false' );
-/**
- * Custom image header
- *
- *
- *
- *
- *
- */
-	add_custom_image_header(
-		'raindrops_header_style',
-		'raindrops_admin_header_style',
-		'raindrops_admin_header_image'
-	);
-	register_default_headers( array(
-		'default' => array(
-			'url' => get_stylesheet_directory_uri().'/images/headers/wp3.jpg',
-			'thumbnail_url' => get_stylesheet_directory_uri().'/images/headers/wp3-thumbnail.jpg',
-			/* translators: header image description */
-			'description' => __( 'Raindrops', 'Raindrops')
-		)
-	) );
-/**
- *
- *
- *
- *
- *
- */
-    add_action('load-themes.php', 'raindrops_install_navigation');
-/**
- *
- *
- *
- *
- *
- */
-    add_editor_style();
-/**
- *
- *
- *
- *
- *
- */
-    register_nav_menus( array(
-        'primary' => __( 'Primary Navigation', 'Raindrops' ),
-    ) );
-/**
- *
- *
- *
- *
- *
- */
-    add_custom_background();
-/**
- *
- *
- *
- *
- *
- */
-    add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 48, 48, true );
-    add_image_size( 'single-post-thumbnail', RAINDROPS_SINGLE_POST_THUMBNAIL_WIDTH, RAINDROPS_SINGLE_POST_THUMBNAIL_HEIGHT, true);
-/**
- *
- *
- *
- *
- *
- */
-    add_theme_support( 'automatic-feed-links' );
-/**
- *
- *
- *
- *
- *
- */
-    load_textdomain( 'Raindrops', get_template_directory().'/languages/'.get_locale().'.mo' );
-/**
- *
- *
- *
- *
- *
- */
-    add_filter("wp_head","raindrops_embed_meta",'99');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter( 'comment_form_default_fields','raindrops_comment_form');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter( 'the_meta_key', 'raindrops_filter_explode_meta_keys', 10, 2 );
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('body_class','raindrops_add_body_class');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('contextual_help','raindrops_help');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('comment_form_field_comment','custom_remove_aria_required1');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('comment_form_default_fields', 'custom_remove_aria_required2');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter( 'the_meta_key', 'raindrops_filter_explode_meta_keys', 10, 2 );
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('the_title','raindrops_fallback_title');
-/**
- *
- *
- *
- *
- *
- */
-    add_filter('the_content','raindrops_ie_height_expand_issue');
-/**
- *
- *
- *
- *
- *
- */
-    if ( !is_admin()) {
-       add_action('wp_print_styles', 'add_raindrops_stylesheet');
-    }
-/**
- *
- *
- *
- *
- *
- */
-    add_action( 'admin_init', 'raindrops_options_init' );
-/**
- *
- *
- *
- *
- *
- */
-     add_action('admin_menu', array($is_submenu, 'add_menus'));
+    $raindrops_functions_file_path = get_stylesheet_directory().'/lib/hooks.php';
+        if(!in_array('alias_functions.php',$included_files)
+            and file_exists($raindrops_functions_file_path)){
+            require_once($raindrops_functions_file_path);
+        }elseif(!in_array('alias_functions.php',$included_files)){
+            require_once(get_template_directory().'/lib/hooks.php');
+        }
+
+/*----- Copy this area and paste Child functions.php when functions customize / End------*/
+
 /**
  * Your extend function , settings write below.
  *
@@ -265,6 +89,12 @@
  *
  *
  */
+ 
+ 
+ 
+ 
+ 
+ 
 /**
  * Original page width implementation by manual labor
  *

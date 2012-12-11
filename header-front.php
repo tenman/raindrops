e0eb79d30eac2a341709d0978ab6b9975a9c018c
tenman <a.tenman@gmail.com>
2012-12-11 17:45:58 +0900

    modified:   header-front.php

--- header-front.php
+++ header-front.php
@@ -5,34 +5,8 @@
  * Displays all of the <head> section and everything up till <div id="bd">
  *
  * @package Raindrops
- * @since Raindrops 0.1
+ * @since Raindrops 0.997
  *
- * @uses get_bloginfo( 'charset' )
- * @uses language_attributes('xhtml')
- * @uses bloginfo('html_type')
- * @uses bloginfo( 'charset' )
- * @uses wp_title( '|', true, 'right' )
- * @uses bloginfo( 'name' ) 
- * @uses get_bloginfo( 'description', 'display' ) 
- * @uses bloginfo( 'pingback_url' )
- * @uses is_singular()
- * @uses get_option( 'thread_comments' )
- * @uses wp_enqueue_script( 'comment-reply' )
- * @uses wp_head()
- * @uses body_class($this_blog)
- * @uses raindrops_warehouse('raindrops_page_width')
- * @uses raindrops_warehouse('raindrops_col_width')
- * @uses wp_upload_dir()
- * @uses raindrops_upload_image_parser($header_image_uri,'inline','#hd')
- * @uses get_theme_mod('header_textcolor', HEADER_TEXTCOLOR)
- * @uses get_header_textcolor()
- * @uses preg_match("!([0-9a-f]{6}|[0-9a-f]{3})!si",get_header_textcolor())
- * @uses home_url()
- * @uses esc_attr()
- * @uses get_bloginfo( 'name', 'display' )
- * @uses raindrops_header_image($args = array()) 
- * 
- * 
  */
 echo '<'.'?'.'xml version="1.0" encoding="'.get_bloginfo( 'charset' ).'"'.'?'.'>'."\n";?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -85,38 +59,50 @@ echo raindrops_site_description();
 if( raindrops_warehouse( 'raindrops_show_menu_primary' ) == "show" ){ ?>
 <div id="access">
 <div class="skip-link screen-reader-text"><a href="#container" title="<?php esc_attr_e( 'Skip to content', 'Raindrops' ); ?>"><?php _e( 'Skip to content', 'Raindrops' ); ?></a></div>
-<?php 
+<?php
 wp_nav_menu( array('container_class' => 'menu-header', 'theme_location' => 'primary') ); ?>
 </div>
 <br class="clear" />
 <?php raindrops_after_nav_menu();?>
 </div>
 <?php  }//raindrops_warehouse( 'raindrops_show_menu_primary' ) ?>
-<div class="yui-g">
-<div class="yui-u first">
-    <div class="static-front-content">
-<?php	if(have_posts()){
-			while (have_posts()){
-					the_post();
-					the_content();
-			}
-		}?>
-        <br style="clear:both" />
 
-      <?php //add nest grid here ?>
+<?php
+$raindrops_header_image = raindrops_header_image( 'elements' );
+if( ! empty( $raindrops_header_image ) or has_post_thumbnail() ){
+?>
+<div class="yui-g">
+    <div class="yui-u first">
+        <div class="static-front-content">
+    <?php   if(have_posts()){
+                while (have_posts()){
+                        the_post();
+                        the_content();
+                }
+            }?>
+            <br style="clear:both" />
+        </div>
     </div>
-</div>
 <div class="yui-u">
 <div class="static-front-media">
 <?php
-/**
- * header image
- * array("style"=>"border:3px solid red;",'img' => 'replace your image uri','color' => 'hexcolor not need #','description' => 'replace your text','description_style' => 'desctiption style rule')
- *
- *
- *
- */
-echo raindrops_header_image( 'elements' );
+    if ( has_post_thumbnail() ){
+        the_post_thumbnail('large','style=max-width:100%;height:auto;');
+    }else{
+        echo $raindrops_header_image;
+    }
+}else{
+?>  <div class="static-front-content">
+<?php   if(have_posts()){
+            while (have_posts()){
+                    the_post();
+                    the_content();
+            }
+        }?>
+        <br style="clear:both" />
+    </div>
+<?php
+}
 ?>
 </div>
 </div>
